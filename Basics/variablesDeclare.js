bd0ff588b3a6fb255d2fe4f1621964bfc029d4e1
regno01 <212147545+regno001@users.noreply.github.com>
2025-12-21 20:07:27 +0530

    Update basics

---
+++ Basics/variablesDeclare.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accouctEmaile = "Regno@gmail.com"
+var accountPassword="12345"
+accountCity = "Jaipur"
+
+// accountId =2
+console.table([accouctEmaile, accountId ,accountPassword,accountCity])
+
+accouctEmaile = "Rahul@1234.com"
+accountPassword = "4321"
+accountCity = "Mumbai"
+
+console.table([accouctEmaile, accountId ,accountPassword,accountCity])
