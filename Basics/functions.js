90b477ecc1ba261fe02abd028a20bd4b128f0e34
regno01 <212147545+regno001@users.noreply.github.com>
2025-12-23 22:27:43 +0530

    Fuvtion fully

--- Basics/functions.js
+++ Basics/functions.js
@@ -2,8 +2,8 @@ function createCourse(coursename){
   console.log('creating '+coursename);
 }
 
-createCourse('hld');
-createCourse('bca');
+// createCourse('hld');
+// createCourse('bca');
 
 function addition(a ,b){
   console.log('addition ' + a+b);
@@ -28,11 +28,34 @@ const multi=(a,b)=>{
   console.log("multiply "+a*b);
   
 }
+const printHeloo = ()=>console.log("hello");
 
-
-multi(2,4)
-sum(3,4);
-addition(2,3);
-minus(7,3);
-multiply(4,5);
-divide(10,2);
+function countVowels(str){
+  let count =0 ;
+  for (const char of str) {
+    if(char=="a"||char=="e"||char=="i"||char=="o" || char=="u"){
+       count++;
+    }
+    
+  }
+  console.log("number od vowels in the string are "+ count);
+}
+countVowels("rahul");
+//arrowfunction to do the same task 
+const countinGVowel=(str)=>{
+  let count=0;
+  for(const char of str){
+    if(char=="a"||char=="e"||char=="i"||char=="o" || char=="u"){
+      count++;
+    }
+  }
+  console.log(count);
+}
+countinGVowel("anksuh");
+// printHeloo();
+// multi(2,4);
+// sum(3,4);
+// addition(2,3);
+// minus(7,3);
+// multiply(4,5);
+// divide(10,2);
