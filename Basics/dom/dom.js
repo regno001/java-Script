84b60824c17796f554554b49ed4cb8bce6a4e13e
regno01 <212147545+regno001@users.noreply.github.com>
2025-12-27 22:57:52 +0530

    manipulation

--- Basics/dom/dom.js
+++ Basics/dom/dom.js
@@ -1,3 +1,6 @@
-//document object object mode
-document.getElementById("header");
-let btn =document.getElementsByClassName("Btn");
+let h2 = document.querySelector("h2");
+h2.innerText= h2.innerText+ " from Apna college student";
+let divs = document.querySelectorAll(".box");
+divs[0].innerText = "new Unique value 1";
+divs[1].innerText="working here";
+divs[2].innerText="fully done";
